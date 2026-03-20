b7d234d3ba311ef6a7a7264727d453b09c19d288
Minindu Madhawa <minidumadawa259@gmail.com>
2026-03-20 08:58:35 +0530

    setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+    <Home></Home>
   );
 }
 
