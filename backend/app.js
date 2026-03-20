afb73c0cf897584cbd8ac17e44faaf4f1a4e6718
Minindu Madhawa <minidumadawa259@gmail.com>
2026-03-20 08:52:53 +0530

    setup

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,15 @@
-console.log("byee");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use("/",(req, res, next) => {
+    res.send("It is Working");
+})
+
+mongoose.connect("mongodb+srv://admin:e6ZvsW0yABwfoCmd@cluster0.hohby0x.mongodb.net/")
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)));
